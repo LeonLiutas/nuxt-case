08cc0ab7d32b569dbef4b7786eb3f370fabcbae3
Leon Strötker <leon@liutas.nl>
2025-08-14 21:48:48 +0200

    Add some basic setup/styling

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <header class="bg-white px-8 py-4 border-b border-gray-200 shadow-md/5">
+        <h1 class="text-3xl font-bold text-primary">REBO Groep</h1>
+    </header>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="min-h-screen bg-gray-100">
+        <AppHeader />
+
+        <main class="p-8">
+            <slot />
+        </main>
+    </div>
+</template>
