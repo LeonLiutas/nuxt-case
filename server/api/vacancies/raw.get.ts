a678bc58480f128b8b5004113426b616b4e23b29
Leon Strötker <leon@liutas.nl>
2025-08-15 21:53:27 +0200

    Add separate API for raw vacancies results to make the external api call cacheable

---
+++ server/api/vacancies/raw.get.ts
@@ -0,0 +1,14 @@
+export default cachedEventHandler(async (event) => {
+    console.log('Fetching external vacancies...');
+
+    // get API URL from config
+    const { apiUrl } = useRuntimeConfig()
+
+    // Do the actual API call
+    const { offers } = await $fetch<{ offers: object[] }>(apiUrl);
+
+    return offers;
+}, {
+    maxAge: 300,
+    swr: true,
+})
